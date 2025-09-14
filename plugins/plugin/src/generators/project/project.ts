76aa77e3c63a1d3635cb08540131b480fb2e0a61
beemood <robert.brightline@gmail.com>
2025-09-14 15:20:15 -0500

    feat: library project generator is done

--- plugins/plugin/src/generators/project/project.ts
+++ plugins/plugin/src/generators/project/project.ts
@@ -1,6 +1,37 @@
-import { formatFiles, generateFiles, names, readJsonFile, Tree, workspaceRoot } from '@nx/devkit';
+import { formatFiles, generateFiles, names, readJsonFile, Tree, updateJson } from '@nx/devkit';
 import { join } from 'path';
 import { ProjectGeneratorSchema } from './schema';
+
+
+async function addTsconfigReference(tree: Tree, projectDirectory: string) {
+  await updateJson(tree, 'tsconfig.json', (value) => {
+    if (value.references == undefined) {
+      value.references = []
+    }
+
+    {
+      const references = value.references;
+
+      const path = `./${projectDirectory}`;
+
+      if (references.find((e: { path: string; }) => e.path === path)) {
+        return value
+      } else {
+        references.push({ path })
+      }
+
+      return value;
+    }
+  })
+}
+
+function getOrganizationPrefix(projectName: string) {
+  return projectName.split(/\//).shift() as string
+}
+
+function getShortName(projectDirectory: string) {
+  return projectDirectory.split(/[/\\]{1,}/).pop() as string;
+}
 
 export async function projectGenerator(
   tree: Tree,
@@ -9,23 +40,28 @@ export async function projectGenerator(
 
   const { directory: projectDirectory, type } = options
   const source = join(__dirname, type);
-  const target = join(workspaceRoot, projectDirectory)
+  const target = projectDirectory
   const mp = await readJsonFile<{ name: string, version: string, homepage: string, author: any, funding: any }>('package.json')
 
-  const shortName = projectDirectory.split(/[/\\]{1,}/).pop()!;
-  const organizationPrefix = mp.name.split(/\//).shift()!;
+  const shortName = getShortName(projectDirectory)
+  const organizationPrefix = getOrganizationPrefix(mp.name);
+
   const projectName = `${organizationPrefix}/${shortName}`
 
   const __names = names(shortName);
 
   generateFiles(tree, source, target, {
-    __names,
+    ...__names,
     projectName,
     projectDirectory,
-    
+    mp
   });
 
+  await addTsconfigReference(tree, projectDirectory)
+
   await formatFiles(tree);
+
+
 }
 
 export default projectGenerator;
