b917485e8b2eecfe70512a4b96657ff7d14e585e
beemood <robert.brightline@gmail.com>
2025-09-25 18:34:28 -0500

    fix: fixed Query decorator Zod validation pipe

--- libs/nest/src/param/query.ts
+++ libs/nest/src/param/query.ts
@@ -1,8 +1,9 @@
 import { Query as NestQuery } from '@nestjs/common';
 import type { ZodObject } from 'zod';
+import { ZodValidationPipe } from '../zod/zod-validation.pipe.js';
 
 export function Query(schema: ZodObject): ParameterDecorator {
   return (...args) => {
-    NestQuery(schema)(...args);
+    NestQuery(new ZodValidationPipe(schema))(...args);
   };
 }
