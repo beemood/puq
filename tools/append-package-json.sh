77267d0fdb949741665e6c5261c4f31c0dd73dc6
beemood <robert.brightline@gmail.com>
2025-09-25 16:02:35 -0500

    feat: license

--- tools/append-package-json.sh
+++ tools/append-package-json.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sed -i "3 a\\$1" libs/**/package.json
-sed -i "3 a\\$1" plugins/**/package.json
+sed -i "3 a\\$1" ../libs/**/package.json
+sed -i "3 a\\$1" ../plugins/**/package.json
+sed -i "3 a\\$1" ../samples/**/package.json
