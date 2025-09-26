0f38eaf032ea5a049d2e104d6d36e609a72de98c
beemood <robert.brightline@gmail.com>
2025-09-25 19:04:15 -0500

    fix: fixed prisma exception filter

---
+++ libs/nest/src/filters/create-prisma-exception-filter.ts
@@ -0,0 +1,35 @@
+import type { ExceptionFilter, Type } from '@nestjs/common';
+import {
+  Catch,
+  InternalServerErrorException,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import type { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+
+export function createPrismaExceptionFilter(
+  prismaClientKnownRequestError: Type
+) {
+  @Catch(prismaClientKnownRequestError)
+  class PrismaExceptionFilter implements ExceptionFilter {
+    catch(exception: PrismaClientKnownRequestError) {
+      switch (exception.code) {
+        case 'P2002': {
+          throw new UnprocessableEntityException({
+            ...exception,
+          });
+        }
+        case 'P2025': {
+          throw new NotFoundException({
+            ...exception,
+          });
+        }
+        default: {
+          throw new InternalServerErrorException('Prisma Exception');
+        }
+      }
+    }
+  }
+
+  return PrismaExceptionFilter;
+}
