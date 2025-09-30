7cf597a2599b937f17afd1ff800e220c9621ceaf
beemood <robert.brightline@gmail.com>
2025-09-30 14:57:38 -0500

    feat: updated the prisma project generator with the most current prisma database project

--- dbs/inventory-db/generate-schemas.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { generateZodSchemaFiles } from '@puq/prisma-schema';
-import { Prisma } from './generated';
-
-generateZodSchemaFiles(Prisma.dmmf.datamodel as any, 'src/lib');
