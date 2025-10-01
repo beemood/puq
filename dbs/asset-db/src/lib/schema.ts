bfbbca92ed30ad7c96ce732b643301caf159f4c4
beemood <robert.brightline@gmail.com>
2025-10-01 18:17:56 -0500

    feat: refiend teh asset database by remove external features such as maintance, tasks and supplier info. They will be implemented internal rotuine service

--- dbs/asset-db/src/lib/schema.ts
+++ dbs/asset-db/src/lib/schema.ts
@@ -1,1 +1,48 @@
-// Generated schemas
+
+import { toOrderBySchema, toWhereQuerySchema } from '@puq/zod';
+import {z} from 'zod';
+    
+
+// ---------- Department Schemas ----------
+
+
+export const DepartmentSchema = z.object({
+  id: z.number().int(),
+  name: z.string(),
+});
+
+export const DepartmentCreateSchema = z.object({
+  name: z.string(),
+});
+
+export const DepartmentUpdateSchema = z.object({
+  id: z.number().int().optional(),
+  name: z.string().optional(),
+});
+
+export const DepartmentWhereSchema = toWhereQuerySchema(DepartmentSchema);
+
+export const DepartmentOrderSchema =  toOrderBySchema(DepartmentSchema);
+
+export const DepartmentSelectSchema = z.object({
+  id: z.boolean().optional(),
+  name: z.boolean().optional(),
+});
+
+export const DepartmentQuerySchema = z.object({
+  take: z.coerce.number().int().min(1), 
+  skip: z.coerce.number().int().min(0), 
+  where: DepartmentWhereSchema.optional(),
+  orderBy: DepartmentOrderSchema.optional(),
+  select: DepartmentSelectSchema.optional()
+});
+
+export type Department = z.infer<typeof DepartmentSchema>;
+export type DepartmentCreate = z.infer<typeof DepartmentCreateSchema>;
+export type DepartmentUpdate = z.infer<typeof DepartmentUpdateSchema>;
+export type DepartmentWhere = z.infer<typeof DepartmentWhereSchema>;
+export type DepartmentOrder = z.infer<typeof DepartmentOrderSchema>;
+export type DepartmentSelect = z.infer<typeof DepartmentSelectSchema>;
+export type DepartmentQuery = z.infer<typeof DepartmentQuerySchema>;
+
+
