5964747198304bfe38980bddc889aec6e71b9a23
beemood <robert.brightline@gmail.com>
2025-10-07 14:03:20 -0500

    feat: clean up the old migration scrips

--- dbs/inventory-db/prisma/migrations/20251002175816_updated/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `taxrate` on the `PriceLevel` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,30)` to `Decimal(10,4)`.
-
-*/
--- AlterTable
-ALTER TABLE "public"."PriceLevel" ALTER COLUMN "taxrate" SET DATA TYPE DECIMAL(10,4);
--- dbs/inventory-db/prisma/migrations/20251007190239_initial/migration.sql
+++ dbs/inventory-db/prisma/migrations/20251007190239_initial/migration.sql
@@ -1,5 +1,11 @@
 -- CreateEnum
-CREATE TYPE "public"."DiscountType" AS ENUM ('PERCENT', 'FIXED_AMOUNT', 'BOGO');
+CREATE TYPE "public"."DiscountValueType" AS ENUM ('PERCENT', 'FIXED');
+
+-- CreateEnum
+CREATE TYPE "public"."DiscountType" AS ENUM ('SIMPLE', 'VOLUME', 'FREE_SHIPPING', 'ORDER_TOTAL');
+
+-- CreateEnum
+CREATE TYPE "public"."TimeUnit" AS ENUM ('SECOND', 'MINUTE', 'HOUR', 'DAY', 'MONTH', 'YEAR', 'DECADE');
 
 -- CreateTable
 CREATE TABLE "public"."Category" (
@@ -14,11 +20,10 @@ CREATE TABLE "public"."Category" (
 -- CreateTable
 CREATE TABLE "public"."Product" (
     "id" SERIAL NOT NULL,
-    "warrantyId" INTEGER,
     "uuid" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "slug" TEXT NOT NULL,
-    "description" TEXT,
+    "description" TEXT DEFAULT 'No description',
 
     CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
 );
@@ -29,7 +34,7 @@ CREATE TABLE "public"."Image" (
     "productId" INTEGER NOT NULL,
     "url" TEXT NOT NULL,
     "order" INTEGER NOT NULL DEFAULT 0,
-    "description" TEXT,
+    "description" TEXT DEFAULT 'No description',
 
     CONSTRAINT "Image_pkey" PRIMARY KEY ("id")
 );
@@ -37,25 +42,13 @@ CREATE TABLE "public"."Image" (
 -- CreateTable
 CREATE TABLE "public"."Supplier" (
     "id" SERIAL NOT NULL,
-    "orgId" TEXT NOT NULL,
+    "uuid" TEXT NOT NULL,
     "productId" INTEGER,
     "supplierSku" TEXT NOT NULL,
-    "supplierCost" DECIMAL(65,30),
+    "supplierCost" DECIMAL(10,2),
     "leadTimeDays" INTEGER,
 
     CONSTRAINT "Supplier_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "public"."PaymentTerm" (
-    "id" SERIAL NOT NULL,
-    "code" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "daysDue" INTEGER NOT NULL,
-    "discountDays" INTEGER NOT NULL,
-    "discountPercent" DECIMAL(65,30) NOT NULL,
-
-    CONSTRAINT "PaymentTerm_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -72,7 +65,6 @@ CREATE TABLE "public"."Variant" (
     "id" SERIAL NOT NULL,
     "uuid" TEXT NOT NULL,
     "productId" INTEGER NOT NULL,
-    "warrantyId" INTEGER,
     "sku" TEXT NOT NULL,
     "upc" TEXT NOT NULL,
 
@@ -80,9 +72,20 @@ CREATE TABLE "public"."Variant" (
 );
 
 -- CreateTable
-CREATE TABLE "public"."Attribute" (
+CREATE TABLE "public"."AttributeCategory" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+
+    CONSTRAINT "AttributeCategory_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Attribute" (
+    "id" SERIAL NOT NULL,
+    "categoryId" INTEGER,
+    "name" TEXT NOT NULL,
+    "description" TEXT DEFAULT 'No description',
 
     CONSTRAINT "Attribute_pkey" PRIMARY KEY ("id")
 );
@@ -91,6 +94,7 @@ CREATE TABLE "public"."Attribute" (
 CREATE TABLE "public"."Unit" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
+    "symbol" TEXT NOT NULL,
 
     CONSTRAINT "Unit_pkey" PRIMARY KEY ("id")
 );
@@ -107,7 +111,7 @@ CREATE TABLE "public"."AttributeUnit" (
 -- CreateTable
 CREATE TABLE "public"."AttributeValue" (
     "id" SERIAL NOT NULL,
-    "unitId" INTEGER,
+    "attributeId" INTEGER NOT NULL,
     "variantId" INTEGER NOT NULL,
     "textValue" TEXT,
     "booleanValue" BOOLEAN,
@@ -121,6 +125,7 @@ CREATE TABLE "public"."Currency" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "code" TEXT NOT NULL,
+    "symbol" TEXT,
 
     CONSTRAINT "Currency_pkey" PRIMARY KEY ("id")
 );
@@ -130,7 +135,9 @@ CREATE TABLE "public"."PriceLevel" (
     "id" SERIAL NOT NULL,
     "currencyId" INTEGER NOT NULL,
     "name" TEXT NOT NULL,
-    "taxrate" DECIMAL(65,30) NOT NULL,
+    "slug" TEXT NOT NULL,
+    "taxrate" DECIMAL(10,4) NOT NULL,
+    "notes" TEXT,
 
     CONSTRAINT "PriceLevel_pkey" PRIMARY KEY ("id")
 );
@@ -140,9 +147,9 @@ CREATE TABLE "public"."Price" (
     "id" SERIAL NOT NULL,
     "variantId" INTEGER NOT NULL,
     "priceLevelId" INTEGER NOT NULL,
-    "price" DECIMAL(65,30) NOT NULL,
-    "cost" DECIMAL(65,30) NOT NULL,
-    "description" TEXT,
+    "price" DECIMAL(10,2) NOT NULL,
+    "cost" DECIMAL(10,2) NOT NULL,
+    "description" TEXT DEFAULT 'No description',
 
     CONSTRAINT "Price_pkey" PRIMARY KEY ("id")
 );
@@ -174,11 +181,16 @@ CREATE TABLE "public"."Discount" (
     "id" SERIAL NOT NULL,
     "code" TEXT NOT NULL,
     "type" "public"."DiscountType" NOT NULL,
-    "value" DECIMAL(65,30) NOT NULL,
+    "valueType" "public"."DiscountValueType" NOT NULL,
+    "value" DECIMAL(10,2),
     "minQuantity" INTEGER,
-    "startDate" TIMESTAMP(3) NOT NULL,
-    "endDate" TIMESTAMP(3) NOT NULL,
-    "maxUses" INTEGER,
+    "maxQuantity" INTEGER,
+    "minOrderTotal" DECIMAL(10,2),
+    "maxOrderTotal" DECIMAL(10,2),
+    "startDate" TIMESTAMP(3),
+    "endDate" TIMESTAMP(3),
+    "usageLimit" INTEGER,
+    "usageCount" INTEGER NOT NULL DEFAULT 0,
 
     CONSTRAINT "Discount_pkey" PRIMARY KEY ("id")
 );
@@ -191,6 +203,7 @@ CREATE TABLE "public"."DiscountTarget" (
     "variantId" INTEGER,
     "storeId" INTEGER,
     "priceLevelId" INTEGER,
+    "categoryId" INTEGER,
 
     CONSTRAINT "DiscountTarget_pkey" PRIMARY KEY ("id")
 );
@@ -201,7 +214,7 @@ CREATE TABLE "public"."Store" (
     "priceLevelId" INTEGER NOT NULL,
     "name" TEXT NOT NULL,
     "slug" TEXT NOT NULL,
-    "description" TEXT,
+    "description" TEXT DEFAULT 'No description',
 
     CONSTRAINT "Store_pkey" PRIMARY KEY ("id")
 );
@@ -210,10 +223,21 @@ CREATE TABLE "public"."Store" (
 CREATE TABLE "public"."WarrantyPolicy" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "description" TEXT,
-    "durationDays" INTEGER NOT NULL,
+    "description" TEXT DEFAULT 'No description',
+    "duration" INTEGER NOT NULL,
+    "durationUnit" "public"."TimeUnit" NOT NULL DEFAULT 'YEAR',
 
     CONSTRAINT "WarrantyPolicy_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."ProductWarranty" (
+    "id" SERIAL NOT NULL,
+    "productId" INTEGER,
+    "variantId" INTEGER,
+    "policyId" INTEGER NOT NULL,
+
+    CONSTRAINT "ProductWarranty_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -232,10 +256,7 @@ CREATE UNIQUE INDEX "Product_name_key" ON "public"."Product"("name");
 CREATE UNIQUE INDEX "Product_slug_key" ON "public"."Product"("slug");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Supplier_orgId_key" ON "public"."Supplier"("orgId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "PaymentTerm_code_key" ON "public"."PaymentTerm"("code");
+CREATE UNIQUE INDEX "Supplier_uuid_key" ON "public"."Supplier"("uuid");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Variant_uuid_key" ON "public"."Variant"("uuid");
@@ -247,16 +268,28 @@ CREATE UNIQUE INDEX "Variant_sku_key" ON "public"."Variant"("sku");
 CREATE UNIQUE INDEX "Variant_upc_key" ON "public"."Variant"("upc");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "AttributeCategory_name_key" ON "public"."AttributeCategory"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AttributeCategory_slug_key" ON "public"."AttributeCategory"("slug");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Attribute_name_key" ON "public"."Attribute"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Unit_name_key" ON "public"."Unit"("name");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "AttributeValue_attributeId_variantId_key" ON "public"."AttributeValue"("attributeId", "variantId");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Currency_name_code_key" ON "public"."Currency"("name", "code");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "PriceLevel_name_key" ON "public"."PriceLevel"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PriceLevel_slug_key" ON "public"."PriceLevel"("slug");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Quantity_variantId_storeId_key" ON "public"."Quantity"("variantId", "storeId");
@@ -266,6 +299,9 @@ CREATE UNIQUE INDEX "SerialNumber_serialNumber_key" ON "public"."SerialNumber"("
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Discount_code_key" ON "public"."Discount"("code");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "DiscountTarget_discountId_productId_variantId_storeId_price_key" ON "public"."DiscountTarget"("discountId", "productId", "variantId", "storeId", "priceLevelId", "categoryId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Store_name_key" ON "public"."Store"("name");
@@ -278,9 +314,6 @@ CREATE UNIQUE INDEX "WarrantyPolicy_name_key" ON "public"."WarrantyPolicy"("name
 
 -- AddForeignKey
 ALTER TABLE "public"."Category" ADD CONSTRAINT "Category_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "public"."Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "public"."Product" ADD CONSTRAINT "Product_warrantyId_fkey" FOREIGN KEY ("warrantyId") REFERENCES "public"."WarrantyPolicy"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "public"."Image" ADD CONSTRAINT "Image_productId_fkey" FOREIGN KEY ("productId") REFERENCES "public"."Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -298,7 +331,7 @@ ALTER TABLE "public"."ProductCategory" ADD CONSTRAINT "ProductCategory_categoryI
 ALTER TABLE "public"."Variant" ADD CONSTRAINT "Variant_productId_fkey" FOREIGN KEY ("productId") REFERENCES "public"."Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "public"."Variant" ADD CONSTRAINT "Variant_warrantyId_fkey" FOREIGN KEY ("warrantyId") REFERENCES "public"."WarrantyPolicy"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "public"."Attribute" ADD CONSTRAINT "Attribute_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "public"."AttributeCategory"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "public"."AttributeUnit" ADD CONSTRAINT "AttributeUnit_attributeId_fkey" FOREIGN KEY ("attributeId") REFERENCES "public"."Attribute"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -310,7 +343,7 @@ ALTER TABLE "public"."AttributeUnit" ADD CONSTRAINT "AttributeUnit_unitId_fkey" 
 ALTER TABLE "public"."AttributeValue" ADD CONSTRAINT "AttributeValue_variantId_fkey" FOREIGN KEY ("variantId") REFERENCES "public"."Variant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "public"."AttributeValue" ADD CONSTRAINT "AttributeValue_unitId_fkey" FOREIGN KEY ("unitId") REFERENCES "public"."Unit"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "public"."AttributeValue" ADD CONSTRAINT "AttributeValue_attributeId_fkey" FOREIGN KEY ("attributeId") REFERENCES "public"."Attribute"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "public"."PriceLevel" ADD CONSTRAINT "PriceLevel_currencyId_fkey" FOREIGN KEY ("currencyId") REFERENCES "public"."Currency"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -349,4 +382,16 @@ ALTER TABLE "public"."DiscountTarget" ADD CONSTRAINT "DiscountTarget_storeId_fke
 ALTER TABLE "public"."DiscountTarget" ADD CONSTRAINT "DiscountTarget_priceLevelId_fkey" FOREIGN KEY ("priceLevelId") REFERENCES "public"."PriceLevel"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "public"."DiscountTarget" ADD CONSTRAINT "DiscountTarget_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "public"."Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "public"."Store" ADD CONSTRAINT "Store_priceLevelId_fkey" FOREIGN KEY ("priceLevelId") REFERENCES "public"."PriceLevel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."ProductWarranty" ADD CONSTRAINT "ProductWarranty_productId_fkey" FOREIGN KEY ("productId") REFERENCES "public"."Product"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."ProductWarranty" ADD CONSTRAINT "ProductWarranty_variantId_fkey" FOREIGN KEY ("variantId") REFERENCES "public"."Variant"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."ProductWarranty" ADD CONSTRAINT "ProductWarranty_policyId_fkey" FOREIGN KEY ("policyId") REFERENCES "public"."WarrantyPolicy"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
