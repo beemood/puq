5398438a9bb5ec3b7499c248914adbef55b22eac
beemood <robert.brightline@gmail.com>
2025-10-16 13:12:24 -0500

    docs: documented the abstract resource class

--- .tasks/TODO.md
+++ .tasks/TODO.md
@@ -1,2 +1,62 @@
 # TODO 
 
+- [ ] update the readme `favicon.png` path. Remove the `puq` part becuase the documents are under the libs, dbs,apps, and samples directories NOT puq. 
+
+
+- [ ] The runtime/library does not support "imports", add the import options to it. 
+````json
+"./runtime/library": {
+    ....,
+   "import": {
+        "types": "./generated/runtime/library.d.ts",
+        "node": "./generated/runtime/library.js"
+   }
+}
+
+````
+
+
+
+
+
+- [ ] The current response for Not-Found entity from prisma exception filter is as follows. Optimize this response and hide internal information. 
+````json
+{
+  "code": "P2025",
+  "meta": {
+    "modelName": "Category",
+    "cause": "No record was found for a query."
+  },
+  "clientVersion": "6.17.1",
+  "name": "PrismaClientKnownRequestError"
+}
+````
+
+- [ ] When request contains two of select, omit, and include, the system throw Internal Error (PrismaClientValidationError), optimaize the Zod schemas so that it throws Unprocessable Entity error
+
+- [ ] Grouped Projection schema is missing, the `create` opteration needs such a schema like `{ select: ..., omit:..., include:... }` but the system only provides `QueryOneSchema` which includes where property!. Create a seperate `ModelNameProjectionSchema` that includes `{select,omit, and include}`
+
+
+- [ ] Where proeprty queries does not allow the scalar values but require the FilterObject (IntFilterScheam, StringFilterSchema, ...). Fix the where schemas so that they allow scalar values withtout object schema such as for a string filter, I should be able to just say "some string" instead of providing an object `{ equals:"some string" }`
+
+
+- [ ] The where property in the `QuerySchema and QueryOneSchema` such as `CategoryQuerySchema, CategoryQueryOneSchema` has issue. The x-to-one relations' where options are suppoed to be the target' models' OwnWhereSchema but it has `{ some: WhereSchem, every: WhereSchema, none: WhereSchema }`, which is correct for the x-to-many relations but not one. One must be just `whereSchema` not the some,every, none object.
+
+
+
+
+- [ ] Optimize the zod error object. The current response is as follows. Make sure to throw Unprocessable entity error as an object 
+
+````json 
+{
+  "name": "ZodError",
+  "message": "[\n  {\n    \"expected\": \"boolean\",\n    \"code\": \"invalid_type\",\n    \"path\": [\n      \"select\",\n      \"id\"\n    ],\n    \"message\": \"Invalid input: expected boolean, received number\"\n  }\n]"
+}
+````
+
+
+- [ ] Remove the "findOneBy" ("api/products/:property/:value") operation! It can be done by "findOne" advance query operation GET ("api/product?query....) or (api/products?query....)
+
+
+
+
--- libs/nest/src/classes/abstract-resource-controller.ts
+++ libs/nest/src/classes/abstract-resource-controller.ts
@@ -6,71 +6,166 @@ import type { PropertyValueParam } from '../types/property-value-param.js';
 export abstract class AbstractResourceController
   implements Record<ResourceOperationName, (...params: Any[]) => Any>
 {
-  findOne(query: Any): Any {
-    throw new NIE(`findOne is not implemented: ${query}`);
+  /**
+   *
+   * 🤣 Save a single entity to the database and projection
+   * @param body
+   * @param projection
+   */
+  saveOne(body: Any, projection: Any): Any {
+    throw new NIE(`saveOne is not implemented: ${{ body, projection }}`);
   }
 
-  findOneBy(params: PropertyValueParam, query: Any): Any {
-    throw new NIE(`findOneBy is not implemented: ${{ params, query }}`);
+  /**
+   * 🤣 Find a single entity by id
+   * @param id Unqiue id
+   * @param query query and projection options
+   */
+  findOneById(id: number, projection: Any): Any {
+    throw new NIE(`findOneById is not implemented: ${{ id, projection }}`);
   }
 
-  findOneById(id: number, query: Any): Any {
-    throw new NIE(`findOneById is not implemented: ${{ id, query }}`);
-  }
-
+  /**
+   * 🤣 Find items by advance query filters, orders, and projections
+   * @param query query and projection options
+   */
   findMany(query: Any): Any {
     throw new NIE(`findMany is not implemented: ${query}`);
   }
 
-  findManyBy(params: PropertyValueParam, query: Any): Any {
-    throw new NIE(`findManyBy is not implemented: ${{ params, query }}`);
+  /**
+   * 🤣 Update an entity by id and projection the result
+   * @param id
+   * @param projection
+   * @param body
+   */
+  updateOneById(id: number, projection: Any, body: Any): Any {
+    throw new NIE(
+      `updateOneById is not implemented: ${{ id, projection, body }}`
+    );
   }
 
-  saveOne(body: Any, query: Any): Any {
-    throw new NIE(`saveOne is not implemented: ${{ body, query }}`);
+  /**
+   * 🤣 Delete a single entity by id and projection
+   * @param id
+   * @param projection
+   */
+  deleteOneById(id: number, projection: Any): Any {
+    throw new NIE(`deleteOneById is not implemented: ${{ id, projection }}`);
   }
 
-  saveMany(body: Any, query: Any): Any {
-    throw new NIE(`saveMany is not implemented: ${{ body, query }}`);
+  /**
+   * 😱 Find a single entity by advance query and projection
+   * @param query query and projection options
+   */
+  findOne(query: Any): Any {
+    throw new NIE(`findOne is not implemented: ${query}`);
   }
 
+  /**
+   * 😱 Find a single entity by exact property and value match
+   * @deprecated Do not implement this method, use the findOne or findMany instead.
+   * @param params
+   * @param query query and projection options
+   */
+  findOneBy(params: PropertyValueParam, projection: Any): Any {
+    throw new NIE(`findOneBy is not implemented: ${{ params, projection }}`);
+  }
+
+  /**
+   * 😱 Find items by exact property and value match and projection
+   * @deprecated Do not implement this method, use findMany instead
+   * @param params
+   * @param projection
+   */
+  findManyBy(params: PropertyValueParam, projection: Any): Any {
+    throw new NIE(`findManyBy is not implemented: ${{ params, projection }}`);
+  }
+
+  /**
+   * 😱 Save items to the database and projection
+   * @param body
+   * @param projection
+   */
+  saveMany(body: Any, projection: Any): Any {
+    throw new NIE(`saveMany is not implemented: ${{ body, projection }}`);
+  }
+
+  /**
+   * 😱 Udpate a single entity in database by the advance query and projection
+   * @deprecated Not recommended. user `updateById` operation instead
+   * @param query query and projection options
+   * @param body
+   */
   updateOne(query: Any, body: Any): Any {
     throw new NIE(`updateOne is not implemented: ${{ query, body }}`);
   }
 
-  updateOneBy(params: PropertyValueParam, query: Any, body: Any): Any {
-    throw new NIE(`updateOneBy is not implemented: ${{ params, query, body }}`);
+  /**
+   * 😱 Update an entity by exact property and value
+   * Not recommended use update by id operation instead
+   * @param params
+   * @param projection
+   * @param body
+   */
+  updateOneBy(params: PropertyValueParam, projection: Any, body: Any): Any {
+    throw new NIE(
+      `updateOneBy is not implemented: ${{ params, projection, body }}`
+    );
   }
 
-  updateOneById(id: number, query: Any, body: Any): Any {
-    throw new NIE(`updateOneById is not implemented: ${{ id, query, body }}`);
-  }
-
+  /**
+   * 😱 Update many entities by advance query and projections
+   * @param query query and projection options
+   * @param body
+   */
   updateMany(query: Any, body: Any): Any {
     throw new NIE(`updateMany is not implemented: ${{ query, body }}`);
   }
 
+  /**
+   * Update many entities by exact property and value
+   * @param params
+   * @param query query and projection options
+   * @param body
+   */
   updateManyBy(params: PropertyValueParam, query: Any, body: Any): Any {
     throw new NIE(
       `updateManyBy is not implemented: ${{ params, query, body }}`
     );
   }
 
+  /**
+   * Delete a single entity by advance query and projection
+   * @param query query and projection options
+   */
   deleteOne(query: Any): Any {
     throw new NIE(`deleteOne is not implemented: ${query}`);
   }
 
+  /**
+   * Delete a single entity by exact property and value match and projection
+   * @param params
+   * @param query query and projection options
+   */
   deleteOneBy(params: PropertyValueParam, query: Any): Any {
     throw new NIE(`deleteOneBy is not implemented: ${{ params, query }}`);
   }
-  deleteOneById(id: number, query: Any): Any {
-    throw new NIE(`deleteOneById is not implemented: ${{ id, query }}`);
-  }
-
+  /**
+   * Delete many entities by advance query and projection
+   * @deprecated use `deleteOneById` operation whenever possible
+   * @param query query and projection options
+   */
   deleteMany(query: Any): Any {
     throw new NIE(`deleteMany is not implemented: ${query}`);
   }
 
+  /**
+   * Delete many entities by exact property and value match
+   * @deprecated use `deleteOneById` operation whenever possible
+   * @param params
+   * @param query query and projection options
+   */
   deleteManyBy(params: PropertyValueParam, query: Any): Any {
     throw new NIE(`deleteManyBy is not implemented: ${{ params, query }}`);
   }
